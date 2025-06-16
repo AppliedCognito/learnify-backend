72bfaa6fe1f71950f537103b3a8fbb4551afe1d9
Aswin K <aswinvtk2001@gmail.com>
2025-06-16 20:01:30 +0530

    bugfix(module,submodule):updation

--- controllers/moduleController.js
+++ controllers/moduleController.js
@@ -1,5 +1,5 @@
-import asyncHandler from 'express-async-handler';
-import { Module } from '../models/moduleModel.js';
+import asyncHandler from "express-async-handler";
+import { Module } from "../models/moduleModel.js";
 
 // Create a module
 const createModule = asyncHandler(async (req, res) => {
@@ -16,27 +16,37 @@ const getModules = asyncHandler(async (req, res) => {
     query.subject_id = subject_id;
   }
 
-  const modules = await Module.find(query).populate('subject_id', 'name');
+  const modules = await Module.find(query).populate("subject_id", "name");
   res.json(modules);
 });
 
 // Get module by ID
 const getModuleById = asyncHandler(async (req, res) => {
-  const module = await Module.findById(req.params.id).populate('subject_id', 'name');
+  const module = await Module.findById(req.params.id).populate(
+    "subject_id",
+    "name"
+  );
   if (!module) {
     res.status(404);
-    throw new Error('Module not found');
+    throw new Error("Module not found");
   }
   res.json(module);
 });
 
 // Update module
 const updateModule = asyncHandler(async (req, res) => {
-  const updated = await Module.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  if (!updated) {
+  const { name, subject_id } = req.body;
+
+  const module = await Module.findById(req.params.id);
+  if (!module) {
     res.status(404);
-    throw new Error('Module not found');
+    throw new Error("Module not found");
   }
+
+  if (name) module.name = name;
+  // Optional: allow subject change
+  if (subject_id) module.subject_id = subject_id;
+  const updated = await module.save();
   res.json(updated);
 });
 
@@ -45,15 +55,9 @@ const deleteModule = asyncHandler(async (req, res) => {
   const deleted = await Module.findByIdAndDelete(req.params.id);
   if (!deleted) {
     res.status(404);
-    throw new Error('Module not found');
+    throw new Error("Module not found");
   }
-  res.json({ message: 'Module deleted' });
+  res.json({ message: "Module deleted" });
 });
 
-export {
-  createModule,
-  getModules,
-  getModuleById,
-  updateModule,
-  deleteModule
-};
+export { createModule, deleteModule, getModuleById, getModules, updateModule };
